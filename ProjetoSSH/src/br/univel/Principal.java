3b351b0bcb90a258167db8b7d3f717ead33c67ef
Christopher Gomes <christopher@certto.com.br>
2016-03-17 21:25:32 -0300

    Alteração feita apos a tag

--- ProjetoSSH/src/br/univel/Principal.java
+++ ProjetoSSH/src/br/univel/Principal.java
@@ -3,8 +3,8 @@
 public class Principal {
 
 	public static void main (String[] args){
-		String msg = "Hellou";
-		System.out.println(msg);
+		String msgg = "Hellou";
+		System.out.println(msgg);
 	}
 }
 
