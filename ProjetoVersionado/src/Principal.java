50cc277aff0a21d5ae08ab06fb162cbaf3e1de27
Christopher Gomes <christopher@certto.com.br>
2016-04-28 21:56:27 -0300

    Apos o export

--- ProjetoVersionado/src/Principal.java
+++ ProjetoVersionado/src/Principal.java
@@ -3,6 +3,7 @@ public class Principal {
 	public static void main (String []args){
 		String msg = "Testando";
 		System.out.println(" Algumas Mudanca" +msg);
+		System.out.println("Testando2");
 		
 	}
 
